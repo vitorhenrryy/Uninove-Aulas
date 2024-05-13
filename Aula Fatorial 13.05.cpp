fe2c4e59b575f3c4b92a14e073144b8c42b0b60c
Vitor henrique <146595403+vitorhenrryy@users.noreply.github.com>
2024-05-13 10:28:24 -0300

    Aula 13.05
    Aula 13.05

---
+++ Aula Fatorial 13.05.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	int num = 0;
+	int fatorial = 1;
+	
+	printf("Digite um Numero");
+	scanf("%i", &num);
+	
+	for(int i = 1; i > 1; i--){
+		fatorial = fatorial *1;
+		
+	}
+	
+	printf("%i", fatorial);
+
+}
+
+
+
+
